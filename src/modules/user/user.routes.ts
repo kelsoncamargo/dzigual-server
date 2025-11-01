e3b2178d9c7c67589fc4c869e6c8b7be5938e42d
Kelson Camargo <kelson.m.camargo@gmail.com>
2025-11-01 16:19:13 -0300

    fix: user.routes is ready

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,4 +1,18 @@
-import express, { Request, Response } from 'express';
+/**
+ * @fileoverview Express router that mounts the user sub-router for all user-related endpoints.
+ *
+ * @module user-main-router
+ * @version 1.0.0
+ *
+ * ### Key Setup
+ * - Creates main router and mounts routerUser at root path '/'.
+ *
+ * ### Routes
+ * - Uses routerUser for all user operations (create, get, update, remove).
+ *
+ */
+
+import express from 'express';
 
 import routerUser from './routes/user.route';
 
