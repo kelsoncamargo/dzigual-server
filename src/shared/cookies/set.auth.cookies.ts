be566b8a39656ad9488a4f96ad1d9db43a14f3a9
Kelson Camargo <kelson.m.camargo@gmail.com>
2025-11-01 06:57:53 -0300

    fix: separate auth cookies

--- src/shared/cookies/set.auth.cookies.ts
+++ src/shared/cookies/set.auth.cookies.ts
@@ -1,3 +1,22 @@
+/**
+ * @fileoverview Utility function to set authentication cookies (access and refresh tokens) in the Express response with secure options.
+ *
+ * @module set-auth-cookies
+ * @version 1.0.0
+ *
+ * ### Key Setup
+ * - Determines secure flag based on production environment.
+ * - Sets 'accessToken' cookie with 5-minute maxAge and 'refreshToken' with 7-day maxAge.
+ *
+ * ### Functions
+ * - setAuthCookies(res, accessToken, refreshToken): Sets both auth cookies in the response.
+ *
+ * @param {Response} res - The Express response object.
+ * @param {string} accessToken - The access token string to set.
+ * @param {string} refreshToken - The refresh token string to set.
+ *
+ */
+
 import { Response } from 'express';
 import isProduction from '../isProduction';
 
